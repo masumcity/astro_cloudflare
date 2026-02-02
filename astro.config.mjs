e2c4312de7868eff32c0c5fe2d4d3dd52e19b14a
Masum <masum@Masums-Mac-mini.local>
2026-02-02 10:46:28 +0600

    cloudflare setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   build: {
     inlineStylesheets: "always",
   },
+
+  adapter: cloudflare(),
 });
